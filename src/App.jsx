5908000502fe62e0909d7c96a23f8e428f7410e2
Jamin Ewell <jamin.ewell@yahoo.com>
2021-01-31 20:16:45 -0800

    Update App.jsx
    minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,6 @@ const element = (
       <p>Hi, my name is Jamin.  I am a Software Developer currently residing in the San Diego area. 
           I enjoy going to the beach and eating seafood in my spare time. I look forward to learning 
           awesome things in this course!
-
-
-
-
       </p>
       <a href="https://github.com/JaminEwell">VIEW MY GITHUB REPO</a>
      </div>    
