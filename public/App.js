2ca7557dd1343062cf0fef353a4209465fd4e933
Jamin Ewell <jamin.ewell@yahoo.com>
2021-01-31 18:26:50 -0800

    update  code
    get project functional

---
+++ public/App.js
@@ -0,0 +1,4 @@
+const element = /*#__PURE__*/React.createElement("div", {
+  title: "Outer div"
+}, /*#__PURE__*/React.createElement("h1", null, "Hello World! I work very well"));
+ReactDOM.render(element, document.getElementById('contents'));
