ca2604854cf46d9a049d194d181f21ed75c1789b
Jamin Ewell <jamin.ewell@yahoo.com>
2021-01-31 20:14:43 -0800

    added img, p, a elements
    added final markup and elements to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+
+
+
+
 const element = (
     <div title="Outer div">
-      <h1>Hello World! I work very well</h1>
-    </div>
-  );
+      <h1>Jamin Ewell</h1>
+      <img src="imgs/profile.jpeg"></img>
+      <p>Hi, my name is Jamin.  I am a Software Developer currently residing in the San Diego area. 
+          I enjoy going to the beach and eating seafood in my spare time. I look forward to learning 
+          awesome things in this course!
+
+
+
+
+      </p>
+      <a href="https://github.com/JaminEwell">VIEW MY GITHUB REPO</a>
+     </div>    
+);
+  
+
+  
+  
+  
   
   ReactDOM.render(element, document.getElementById('contents'));
+  //ReactDOM.render(profile, document.getElementById('profileimage'));
